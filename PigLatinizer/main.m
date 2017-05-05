618e5ddcf654d6931f1a7d786909ab82e401a216
Marc Maguire <maguire.marc@gmail.com>
2017-05-05 16:54:12 -0400

    initial commit

---
+++ PigLatinizer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatinizer
+//
+//  Created by Marc Maguire on 2017-05-05.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
